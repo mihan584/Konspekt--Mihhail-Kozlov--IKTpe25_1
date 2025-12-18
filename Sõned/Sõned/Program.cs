c775ea50696fd095a0ee6596d874b76823a81967
Opilane TTHK <opilane@tthk.ee>
2025-12-18 09:35:50 +0200

    Sõned

---
+++ Sõned/Sõned/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Sõned
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           namespace Sõned
+    {
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                string lause = "TERe hommikust, mul on moNSTER otsas, KAS ma võin veel magada?";
+                //Console.WriteLine("lause oikkus on: "+lause.Length);
+                // Console.WriteLine("see tekst on capslockis:" + lause.ToUpper());
+                // Console.WriteLine("see tekst on väike tähtedes:" + lause.ToLower());
+                // Console.WriteLine("Kas lauses on sõna \"magada\":" + lause.ToLower().Contains ("magada"));
+
+                //List<string> list = new List<string>() { "auto", "kraav","kruus"};
+                //string listkoos = "";
+                //foreach (var sõne  in list)
+                //{
+                //    listkoos += (" "+sõne.ToUpper()+",");
+                //}
+                //Console.WriteLine(listkoos);
+
+                //string eesnimi = "Julius";
+                //string perekonanimi = "Caesar";
+                //string amet = "Ohver";
+                //string kuuluskeiser = string.Concat(eesnimi, perekonanimi, amet);
+                //Console.WriteLine(kuuluskeiser);
+
+
+                //string kuuluskeiser2 = $"{eesnimi}, {perekonanimi}, {amet}";
+                //Console.WriteLine($"{eesnimi}, {perekonanimi}, {amet}");
+                //Console.WriteLine(kuuluskeiser2);
+
+
+                //string[] sõnedlauses = lause.Split(' ');
+                //foreach (var sõne in sõnedlauses)
+                //{
+                //    Console.WriteLine(sõne);
+                //}
+                //string ükssõna = "kuulilennuteetunneliluuk";
+                //Console.WriteLine(ükssõna[4]);
+                //Console.WriteLine(ükssõna.IndexOf("i"));
+
+                //Console.WriteLine(" \" ");
+
+                //Console.WriteLine("a \nb");
+
+                //Console.WriteLine("AAAA\tBBBB");
+            }
+        }
+    }
+}
+    
+
