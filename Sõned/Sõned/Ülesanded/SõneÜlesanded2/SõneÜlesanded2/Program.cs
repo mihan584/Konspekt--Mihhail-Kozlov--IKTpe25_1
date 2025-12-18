69623dfb16922fb1ee4d040514644a0ce6cd09e3
Opilane TTHK <opilane@tthk.ee>
2025-12-18 10:47:47 +0200

    SõneÜlesanne2

---
+++ Sõned/Sõned/Ülesanded/SõneÜlesanded2/SõneÜlesanded2/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace SõneÜlesanded2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           //kirjuta programm kus on kaks kasutajat kasutajanime ja parooliga
+           //küsi kasutajalt tema kasutajanime, otsi kasutajate järjendist vastav kasutaja, ja kui kasutajat ei leita ütle et kasutaja ei eksisteeri
+           //kui kasutaja eksisteerib, vaata kasutaja parooli, küsi kasutajalt tema parooli ja võrdle neid
+           // ´kui paroolid ühtival, ütle tere tulemast
+           //kui aga ei ühti, küsi kasutajalt parooli uuesti.
+
+            List<List<string>> kasutajad = new List<List<string>>();
+            kasutajad.Add(new List<string> { "kasutajanimi1", "parool1" });
+            kasutajad.Add(new List<string> { "kasutajanimi2", "parool2" });
+            //{
+            //    Console.WriteLine(item.ToString() + item[0] +","+ 
+            //}
+            string kasutajanimi = "";
+            Console.WriteLine("Palun sisesta oma kasutajanimi");
+            kasutajanimi = Console.ReadLine();
+            bool doesuserexist = false;
+            string õigeparool = "";
+            foreach (var user  in kasutajad)
+            {
+                if (user[0] == kasutajanimi)
+                {
+                    doesuserexist = true;
+                    õigeparool = user[1];
+                }
+                {
+                    if (doesuserexist == true)
+                    {
+                        string sisestus = "";
+                        Console.WriteLine("Tere", kasutajanimi, "sisesta oma parool");
+                        while (sisestus != õigeparool)
+                        {
+                            Console.WriteLine("Palun sisesta oma parool:");
+                            sisestus = Console.ReadLine();
+                        }
+                        Console.WriteLine($"Tere tulemast {kasutajanimi}");
+                    }
+                    else
+                    {
+ Console.WriteLine("kasutaja ei eksisteeri");
+                    }
+                        
+                           
+                        
+                        
+                        
+                            
+                        
+                    }
+                }
+            }
+        }
+    }
+}
